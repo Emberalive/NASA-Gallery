8b024e8950ac05742945a25ce264d7aa1b92bba6
samuel <samueltsutcliffe@gmail.com>
2025-03-03 15:31:02 +0000

    strated fix for media loading in the big image card

--- js.js
+++ js.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', async function () {
+    let links = []
     const dropSearch = document.querySelector('#dropSearch');
     const searchButton = document.querySelector('#btn1');
     
@@ -10,20 +11,23 @@ window.addEventListener('load', async function () {
     }
 
     radom_search(dropSearch, searchButton);
-
-    // Handle search button click
-    document.querySelector('#btn1').addEventListener('click', async function () {
+    
+    async function search_nasa() {
         // list for media types selected
         let mediaformat = []
         // getting the media type, and checking if it is checked, and if so pushing it to the media format list
-        if (document.querySelector('#images').checked){mediaformat.push('image')}
-        if (document.querySelector('#videos').checked){mediaformat.push('video')}
+        if (document.querySelector('#images').checked) {
+            mediaformat.push('image')
+        }
+        if (document.querySelector('#videos').checked) {
+            mediaformat.push('video')
+        }
         // setting the main content area to nothing, to reset the images / videos
         main_container.innerHTML = '';
         // getting the search value from the text input
         const query = dropSearch.value.trim();
         // making the css loading bar to be visible
-        document.querySelector("#load_bar").classList.remove ("hide");
+        document.querySelector("#load_bar").classList.remove("hide");
         document.querySelector("#back2top").classList.remove('hide');
 
         // setting the url to incorporate the media values as well as the search query
@@ -38,81 +42,82 @@ window.addEventListener('load', async function () {
             for (const item of obj.collection.items) {
                 const data = item.data[0];
                 const title = data.title;
-                const link = item.links[0].href
-                const descr = data.description; 
+                const link = item.links[0].href;
+                links = item.links;
+                const descr = data.description;
                 const media_type = data.media_type;
 
                 console.log(descr);
                 console.log(link);
                 console.log(media_type);
-                
-                    const main = document.querySelector("#nasa_gallery");
 
-                    const title_div = document.createElement("div");
-                    title_div.classList.add('title');
+                const main = document.querySelector("#nasa_gallery");
 
-                    const Title = document.createElement("h2");
-                    Title.textContent = title;
+                const title_div = document.createElement("div");
+                title_div.classList.add('title');
 
-                    title_div.appendChild(Title);
+                const Title = document.createElement("h2");
+                Title.textContent = title;
 
-                    const div = document.createElement("div");
-                    div.classList.add('text-div');
+                title_div.appendChild(Title);
 
-                    const newDiv = document.createElement("div");
-                    newDiv.classList.add("container");
+                const div = document.createElement("div");
+                div.classList.add('text-div');
 
-                    const description = document.createElement("p");
-                    description.textContent = descr;
+                const newDiv = document.createElement("div");
+                newDiv.classList.add("container");
 
-                    const keywords = document.createElement('div');
-                    keywords.classList.add('hide');
-                    keywords.id = 'keyword';
-                    
-                    if (data.keywords && data.keywords.length > 0) {
-                        const keywordsText = data.keywords.map(keyword => {
-                            return `<a href="#" class="keyword">${keyword}</a>`;
-                        }).join(', ');
-                        keywords.innerHTML = `Keywords: ${keywordsText}`;
-                    } else {
-                        keywords.textContent = 'Keywords: N/A';
-                    }
+                const description = document.createElement("p");
+                description.textContent = descr;
 
-                    newDiv.appendChild(title_div); 
-                    
-                    if (media_type === 'image') {
-                        const newImg = document.createElement("img");
-                        newImg.src = link;
-                        
-                        newDiv.appendChild(newImg);
-                    } else if (media_type === 'video') {
-                        // Make an additional fetch request to get the video URL
-                        const assetUrl = item.href;
-                        const videoResponse = await fetch(assetUrl);
-                        const videoText = await videoResponse.text();
-                        const videoObj = JSON.parse(videoText);
+                const keywords = document.createElement('div');
+                keywords.classList.add('hide');
+                keywords.id = 'keyword';
 
-                        // Assuming the video URL is in the first item of the array
-                        const videoUrl = videoObj[0];
-                        console.log(videoUrl); // Check the URL in the console
+                if (data.keywords && data.keywords.length > 0) {
+                    const keywordsText = data.keywords.map(keyword => {
+                        return `<a href="#" class="keyword">${keyword}</a>`;
+                    }).join(', ');
+                    keywords.innerHTML = `Keywords: ${keywordsText}`;
+                } else {
+                    keywords.textContent = 'Keywords: N/A';
+                }
 
-                        const newVideo = document.createElement("video");
-                        newVideo.src = videoUrl;
-                        newVideo.controls = true; // Add video controls (play, pause, volume)
+                newDiv.appendChild(title_div);
 
-                        newVideo.onerror = () => {
-                            newVideo.style.display = 'none'; // Hide the video if it fails to load
-                        };
-                        
-                        newDiv.appendChild(newVideo);
+                if (media_type === 'image') {
+                    const newImg = document.createElement("img");
+                    newImg.src = link;
 
-                    }
-                    
-                    div.appendChild(description);
-                    div.appendChild(keywords);
-                    newDiv.appendChild(div);
-                    main.appendChild(newDiv);
-                    document.querySelector("#load_bar").classList.add("hide");
+                    newDiv.appendChild(newImg);
+                } else if (media_type === 'video') {
+                    // Make an additional fetch request to get the video URL
+                    const assetUrl = item.href;
+                    const videoResponse = await fetch(assetUrl);
+                    const videoText = await videoResponse.text();
+                    const videoObj = JSON.parse(videoText);
+
+                    // Assuming the video URL is in the first item of the array
+                    const videoUrl = videoObj[0];
+                    console.log(videoUrl); // Check the URL in the console
+
+                    const newVideo = document.createElement("video");
+                    newVideo.src = videoUrl;
+                    newVideo.controls = true; // Add video controls (play, pause, volume)
+
+                    newVideo.onerror = () => {
+                        newVideo.style.display = 'none'; // Hide the video if it fails to load
+                    };
+
+                    newDiv.appendChild(newVideo);
+
+                }
+
+                div.appendChild(description);
+                div.appendChild(keywords);
+                newDiv.appendChild(div);
+                main.appendChild(newDiv);
+                document.querySelector("#load_bar").classList.add("hide");
             }
             console.log(text);
         } catch (e) {
@@ -124,21 +129,30 @@ window.addEventListener('load', async function () {
             warning.textContent = text;
             warn_div.appendChild(warning);
         }
-        });
-    
+    }
+    // Handle search button click
+    document.querySelector('#btn1').addEventListener('click', search_nasa);
 
+    
+    // loading the main card for one image or video
     const main_container = document.querySelector('#nasa_gallery');
     main_container.addEventListener('click',  function(event) {
+        
+        //checking the number of containers against the list of media items, to se if all the images/ videos have been loaded
+        const num_containers = main_container.querySelectorAll('.container').length;
+        
         const clickedContainer = event.target.closest('.container');
-
-        if (clickedContainer) {
+        const media_num = links.length
+        if (clickedContainer && media_num === num_containers) {
+            const backButton = document.querySelector('#backButton');
             const title = clickedContainer.querySelector('h2').textContent;
             const keywordsDiv = clickedContainer.querySelector('#keyword');
             const imageElement = clickedContainer.querySelector('img');
             const videoElement = clickedContainer.querySelector('video');
             const description = clickedContainer.querySelector('p').innerText;
             const searchButton = document.querySelector('#btn1');
-
+            
+            backButton.classList.remove('hide');
             searchButton.classList.add('hide');
             dropSearch.classList.add('hide');
             main_container.classList.remove('nasa_gallery');
@@ -164,7 +178,7 @@ window.addEventListener('load', async function () {
                                 </div>
                             </div>
                     </div>
-                    <button id="backButton">Go Back</button>
+<!--                    <button id="backButton">Go Back</button>-->
                 </div>
                 `;
             } else {
@@ -181,12 +195,14 @@ window.addEventListener('load', async function () {
                         </div>
                         </div>
                     </div>
-                    <button id="backButton">Go Back</button>
+<!--                    <button id="backButton">Go Back</button>-->
                 </div>
                 `;
             }
             document.querySelector(".wrapper").style.padding = '30px';
             document.querySelector("#back2top").classList.add('hide');
+        } else {
+            console.log("not all media have been loaded, plase wait")
         }
 
         const keywordLinks = document.querySelectorAll('.keyword');
@@ -201,17 +217,18 @@ window.addEventListener('load', async function () {
                 searchButton.click();  // Trigger the search button click   
             });
         });
-
-        
-        const searchButton = document.querySelector('#btn1');
-        document.querySelector('#backButton').addEventListener('click',  function () {
-            main_container.innerHTML = '';
-            searchButton.classList.remove('hide');
-            main_container.classList.add('text');
-            dropSearch.classList.remove('hide');
-            searchButton.click();
-        })
     })
+    
+    const backButton = document.querySelector('#backButton');
+    backButton.addEventListener('click',  function () {
+        backButton.classList.add('hide');
+        main_container.innerHTML = '';
+        searchButton.classList.remove('hide');
+        main_container.classList.add('text');
+        dropSearch.classList.remove('hide');
+        searchButton.click();
+    })
+    
     document.querySelector('#back2top').addEventListener('click', function () {
         window.scrollTo({ top: 0, behavior: 'smooth' }); // Smooth scroll to top
     });
